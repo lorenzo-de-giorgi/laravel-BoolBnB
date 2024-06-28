56af4df24d9ad7da1a5de2949c9b75c8a6ccd93f
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-06-28 20:03:07 +0200

    detail apartment page

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -5,8 +5,32 @@
 @section('content')
     <section>
         <h2>{{$apartment->title}}</h2>
+        <div class="d-flex">
         <img src="{{asset('storage/' . $apartment->image)}}" alt="{{$apartment->title}}">
-       
+        <div>
+            <p>{{$apartment->address}}</p>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">number of rooms</th>
+      <th scope="col">number of beds</th>
+      <th scope="col">number of bathrooms</th>
+      <th scope="col">square meters</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">{{$apartment->rooms_num}}</th>
+      <td>{{$apartment->beds_num}}</td>
+      <td>{{$apartment->bathrooms_num}}</td>
+      <td>{{$apartment->square_meters}}</td>
+    </tr>
+    </tbody>
+    </table>
+    </div>
+    </div>
+   
+
     </section>
 
 @endsection
