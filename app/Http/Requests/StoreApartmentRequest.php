8123e22d52c8b265f786247e3d71599a7605633e
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-07-01 16:27:19 +0200

    Co-authored-by: riccardo marroni <RiccardoMarroni@users.noreply.github.com> Co-authored-by: Degio <lorenzo-de-giorgi@users.noreply.github.com>

---
+++ app/Http/Requests/StoreApartmentRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreApartmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|min:5|max:255',
+            'beds_num' => 'required|numeric',
+            'rooms_num' => 'required|numeric',
+            'bathrooms_num' => 'required|numeric',
+            'square_meters' => 'required|numeric',
+            'address' => 'min:5|max:255',          
+            'visibility' => 'required|boolean',
+            'image' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Title field is required!',
+            'title.unique:apartments' => 'This title already exist!',
+            'title.max' => 'The length of the title must not exceed :max characters!',
+            'title.min' => 'The length of the title must be at least :min characters!',
+            'image.max' => 'The weight of the image cannot exceed 1MB!'
+        ];
+    }
+}
