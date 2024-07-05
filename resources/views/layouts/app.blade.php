f52addc08fc15590c581d756a4c6276ef31115e8
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-05 18:20:34 +0200

    replaced header with sidebar menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,72 +20,93 @@
 
 <body>
     <div id="app">
-
-
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-            <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('http://localhost:5173') }}">
-                    <div id="adminLogo">
-                        <img src="{{ asset('img/adminLogo.png') }}" alt="Admin Logo">                        
-                    </div>
-                    {{-- config('app.name', 'Laravel') --}}
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('/admin/apartments') }}">{{ __('Apartments') }}</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                            @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                            @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('admin/dashboard') }}">{{__('Dashboard')}}</a>
-                                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+        <div class="container-fluid">
+            <div class="row flex-nowrap">
+                <div class="col-auto px-sm-2 px-0 bg-dark">
+                    <div
+                        class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                        <a class="navbar-brand d-flex align-items-center" href="{{ url('http://localhost:5173') }}">
+                            <div id="adminLogo">
+                                <img src="{{ asset('img/adminLogo.png') }}" alt="Admin Logo">
                             </div>
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                            {{-- config('app.name', 'Laravel') --}}
+                        </a>
+                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+                            id="menu">
+                            <li>
+                                <a href="#submenu1" data-bs-toggle="collapse"
+                                    class="nav-link px-0 align-middle link-light">
+                                    <i class="fs-4 bi-speedometer2 ms-1"></i>
+                                    <span class="ms-1 d-none d-sm-inline">
+                                        Admin Dashboard
+                                    </span>
+                                </a>
+                                <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                    <li class="w-100">
+                                        <a href="{{url('/') }}" class="nav-link px-0 link-light">
+                                            <i class="fa-solid fa-house"></i>
+                                            <span class="ms-1 d-none d-sm-inline">
+                                                {{ __('Home') }}
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li class="w-100">
+                                        <a href="{{url('admin/apartments') }}" class="nav-link px-0 link-light">
+                                            <i class="fa-solid fa-building ms-1"></i>
+                                            <span class="ms-1 d-none d-sm-inline">
+                                                {{ __('Apartments') }}
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <hr>
+                        @guest
+                            <ul>
+                                <li class="dropdown pb-4 list-unstyled">
+                                    <a class="d-flex align-items-center text-white text-decoration-none"
+                                        href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if(Route::has('register'))
+                                    <li class="dropdown pb-4 list-unstyled">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                        @else
+                                <li class="dropdown pb-4 list-unstyled">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <img src="https://cdn.noitatnemucod.net/avatar/100x100/one_piece/user-08.jpeg"
+                                            alt="One Piece - Luffy" width="30" height="30" class="rounded-circle">
+                                        <span id="userName">
+                                            {{ Auth::user()->name }}
+                                        </span>
+                                    </a>
+                                    <div class=" dropdown-menu dropdown-menu-dark text-small shadow"
+                                        aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ url('admin/dashboard') }}">{{__('Dashboard')}}</a>
+                                        <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                                        <hr class="dropdown-divider">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
-        <main class="">
-            @yield('content')
-        </main>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            </ul>
+                        @endguest
+                    </div>
+                </div>
+                <main class="col vh-100 overflow-y-auto">
+                    @yield('content')
+                </main>
+            </div>
+        </div>
     </div>
 </body>
 
