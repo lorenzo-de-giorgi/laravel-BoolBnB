7a8d489801253f6d4c63160b0c087671f27a4547
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-09 12:49:53 +0200

    minor fix

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -6,8 +6,7 @@
 <section>
     <div class="container">
         <h2>Create New Apartment</h2>
-        <form action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data"
-            onsubmit="return validaForm()">
+        <form action="{{ route('admin.apartments.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             {{-- title --}}
             <div class="mb-3">
