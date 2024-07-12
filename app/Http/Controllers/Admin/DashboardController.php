84541ee934bc702e18921242933ee8c43acae8b7
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-07-12 19:45:19 +0200

    dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\View;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('admin.dashboard');
+    public function index()
+    {
+        $userId = Auth::id();
+        $views = View::whereHas('apartment', function($query) use ($userId){
+            $query->where('user_id', $userId);
+        })->get();
+        return view('admin.dashboard', compact('views'));
     }
 }
