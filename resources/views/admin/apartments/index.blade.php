3d2760f3cba9987c085d21e4b3cfae377da255ab
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-14 17:21:09 +0200

    rework on main table: fix responsive change layout

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -6,24 +6,18 @@
 
 <section>
   <div class="container p-5">
-    <div class="d-flex justify-content-between align-items-center py-4">
+    <div id="title" class="py-4">
       <h1>Apartments</h1>
       <a href="{{route('admin.apartments.create')}}" class="btn btn-primary">Add new Apartment</a>
     </div>
-    <div class="table100">
+    <div id="mobile">
       <table>
-        <thead>
-          <tr class="table100-head">
-            <th class="column1" scope="col">Image</th>
-            <th class="column2" scope="col">Title</th>
-            <th class="column3" scope="col">Visibility</th>
-            <th class="column4" scope="col">Sponsorship</th>
-            <th class="column5" scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($apartments as $apartment)
-            @php
+        @foreach ($apartments as $apartment)
+          <tbody>
+            <tr>
+            <th class="column1" scope="row">Image</th>
+            <td class="column1body">
+              @php
         $imageArray = json_decode($apartment->image, true);
         $image = '';
         if (!empty($imageArray)) {
@@ -35,17 +29,26 @@
         }
         }
         @endphp
+              <img src="{{asset('storage/' . $image)}}" alt="{{$apartment->title}}" alt="apartment image">
+            </td>
+            </tr>
             <tr>
-            <td class="column1body"><img src="{{asset('storage/' . $image)}}" alt="{{$apartment->title}}"></td>
-            <td class="column2">{{$apartment->title}}</td>
-            <td class="column3">
+            <th class="column2" scope="row">Title</th>
+            <td>{{ $apartment->title }}</td>
+            </tr>
+            <tr>
+            <th class="column3" scope="row">Visibility</th>
+            <td>
               @if ($apartment->visibility == 1)
-                Visible
-              @else
-              Not Visible
-              @endif
-              </td>
-            <td class="column4">
+          Visible
+        @else
+        Not Visible
+      @endif
+            </td>
+            </tr>
+            <tr>
+            <th class="column4" scope="row">Sponsorship</th>
+            <td>
               @if ($apartment->sponsorships)
           @foreach ($apartment->sponsorships as $sponsorship)
         @if ($sponsorship->pivot->end_time > now())
@@ -54,11 +57,14 @@
       @endforeach
         @endif
             </td>
+            </tr>
+            <tr>
+            <th class="column5" scope="row">Actions</th>
             <td class="column5">
-              <div class="d-flex justify-content-end">
+              <div class="d-flex align-items-center">
               <a href="{{route('admin.apartments.show', $apartment->slug)}}"><i class="fa-solid fa-eye"></i></a>
               <a href="{{route('admin.apartments.edit', $apartment->slug)}}"><i class="fa-solid fa-pen"></i></a>
-              <form action="{{route('admin.apartments.destroy', $apartment->slug)}}" method="POST">
+              <form action="{{route('admin.apartments.destroy', $apartment->slug)}}" method="POST" class="m-0">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="delete-button border-0 bg-transparent"
@@ -69,8 +75,8 @@
               </div>
             </td>
             </tr>
-      @endforeach
-        </tbody>
+          </tbody>
+    @endforeach
       </table>
     </div>
   </div>
