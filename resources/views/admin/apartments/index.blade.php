ae63ac60910ea1b44c91babe7ea46ed6570d2fc8
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-07-15 12:43:34 +0200

    fix homepage apartment and some fix to show

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -10,9 +10,9 @@
       <h1>Apartments</h1>
       <a href="{{route('admin.apartments.create')}}" class="btn btn-primary">Add new Apartment</a>
     </div>
-    <div class="row">
-    @foreach ($apartments as $apartment)
-      
+    <div class="row g-5">
+      @foreach ($apartments as $apartment)
+
 
         <!-- COL-5 -->
 
@@ -24,33 +24,40 @@
         foreach ($imageArray as $key => $value) {
         if (!empty($value)) {
         $image = $value;
-        break;}}}
-         @endphp
-        <img src="{{asset('storage/' . $image)}}" alt="{{$apartment->title}}" alt="apartment image" class="imgApartment w-100">
+        break;
+        }
+        }
+        }
+       @endphp
+        <img src="{{asset('storage/' . $image)}}" alt="{{$apartment->title}}" alt="apartment image"
+          class="imgApartment w-100">
         </div>
 
         <!-- COL-5 -->
 
-        <div class="col-sm-12 col-md-6 ">
-        <h2>{{ $apartment->title }}</h2>
-        <p> visibility
-           @if ($apartment->visibility == 1)
-           <i class="fa-solid fa-check" style="color: #4ca456;"></i>
-          @else
-          <i class="fa-solid fa-check" style="color: #4ca456;"></i>
-           @endif
-        </p>
-        <p>
-           @if ($apartment->sponsorships->isNotEmpty())
-           @foreach ($apartment->sponsorships as $sponsorship)
-           @if ($sponsorship->pivot->end_time > now())
-          <img src="/img/{{$sponsorship->badge}}" alt="Sponsorship Badge" style="width: 60px; height: 60px;">
-            @endif
-          @endforeach
-          @else
-         <div class="rounded-pill text-bg-success p-1 text-center">Not Sponsored</div>
-           @endif </p>
-           <div class="d-flex align-items-center">
+        <div class="col-sm-12 col-md-6 d-flex flex-column justify-content-between">
+        <h2 class="text-center">{{ $apartment->title }}</h2>
+        <div class="d-flex align-items-center justify-content-center flex-column">
+          <h4>Available</h4>
+          @if ($apartment->visibility == 1)
+        <i class="fa-solid fa-check fs-2" style="color: #4ca456;"></i>
+      @else
+      <i class="fa-solid fa-xmark fs-2" style="color: #ff0000;"></i>
+    @endif
+        </div>
+        <div class="d-flex align-items-center justify-content-center flex-column mb-3">
+          <h4>Sponsorship</h4>
+          @if ($apartment->sponsorships->isNotEmpty())
+        @foreach ($apartment->sponsorships as $sponsorship)
+      @if ($sponsorship->pivot->end_time > now())
+      <img src="/img/{{$sponsorship->badge}}" alt="Sponsorship Badge" style="width: 100px; height: 100px;">
+    @endif
+    @endforeach
+      @else
+      <div class="rounded-pill text-bg-danger p-1 text-center d-block">Not Sponsored</div>
+    @endif 
+        </div>
+        <div class="column5 d-flex align-items-center justify-content-center">
           <a href="{{route('admin.apartments.show', $apartment->slug)}}"><i class="fa-solid fa-eye"></i></a>
           <a href="{{route('admin.apartments.edit', $apartment->slug)}}"><i class="fa-solid fa-pen"></i></a>
           <form action="{{route('admin.apartments.destroy', $apartment->slug)}}" method="POST" class="m-0">
@@ -63,9 +70,10 @@
           </form>
         </div>
         </div>
-
-        <!-- COL-4 -->
-       <!--  <div class="col-sm-6 col-md-2">
+    @endforeach
+    </div>
+    <!-- COL-4 -->
+    <!--  <div class="col-sm-6 col-md-2">
         <div class="d-flex align-items-center">
           <a href="{{route('admin.apartments.show', $apartment->slug)}}"><i class="fa-solid fa-eye"></i></a>
           <a href="{{route('admin.apartments.edit', $apartment->slug)}}"><i class="fa-solid fa-pen"></i></a>
@@ -73,17 +81,16 @@
           @csrf
           @method('DELETE')
           <button type="submit" class="delete-button border-0 bg-transparent"
-            data-item-title="{{ $apartment->title }}">
-            <i class="fa-solid fa-trash"></i>
+          data-item-title="{{ $apartment->title }}">
+          <i class="fa-solid fa-trash"></i>
           </button>
           </form>
         </div>
         </div> -->
-        
 
-    @endforeach
-    </div>
-      <!--  <div id="mobile">
+
+
+    <!--  <div id="mobile">
       <table>
         @foreach ($apartments as $apartment)
           <tbody>
@@ -154,7 +161,7 @@
     @endforeach
       </table>
     </div> -->
-    </div>
+  </div>
 </section>
 @include('partials.modal-delete')
 @endsection
